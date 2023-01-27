fca010b0d770fa291e97b68098138599bd7cbc11
Avdic Sabahudin <sabahudin.avdic@park-depot.de>
2023-01-27 16:07:14 +0100

    change Window layout and add some type hints

--- open_sub_in_dict/LearnFromSubtitles.py
+++ open_sub_in_dict/LearnFromSubtitles.py
@@ -54,44 +54,54 @@ class BigWindow(QWidget):
 
         self.Deu_cont = QTextEdit(self)
         self.Deu_cont.move(5, 5)
-        self.Deu_cont.resize(490, 295)
+        self.Deu_cont.resize(490, 140)
 
         self.Eng_cont = QTextEdit(self)
-        self.Eng_cont.move(500, 5)
-        self.Eng_cont.resize(490, 295)
+        self.Eng_cont.move(5, 150)
+        self.Eng_cont.resize(490, 145)
 
         self.line = QLineEdit(self)
-        self.line.move(400, 310)
-        self.line.resize(200, 40)
+        self.line.move(5, 355)
+        self.line.resize(200, 45)
+        self.line.setPlaceholderText("type the new word...")
 
-        self.prv_button = QPushButton('Previous', self)
-        self.prv_button.move(5, 350)
+        self.appendPrevious_button = QPushButton('Add Prev', self)
+        self.appendPrevious_button.move(5, 305)
+        self.appendPrevious_button.resize(90, 25)
 
-        self.nxt_button = QPushButton('Next', self)
-        self.nxt_button.move(105, 350)
+        self.jumpPrevious_button = QPushButton('Previous', self)
+        self.jumpPrevious_button.move(105, 305)
+        self.jumpPrevious_button.resize(90, 25)
 
-        self.apr_button = QPushButton('Add Prev', self)
-        self.apr_button.move(205, 350)
+        self.jumpNext_button = QPushButton('Next', self)
+        self.jumpNext_button.move(300, 305)
+        self.jumpNext_button.resize(90, 25)
 
-        self.anx_button = QPushButton('Add Next', self)
-        self.anx_button.move(305, 350)
+        self.appendNext_button = QPushButton('Add Next', self)
+        self.appendNext_button.move(400, 305)
+        self.appendNext_button.resize(90, 25)
 
-        self.sv_button = QPushButton('Save', self)
-        self.sv_button.move(405, 350)
+        self.save_button = QPushButton('Save\nto Dict', self)
+        self.save_button.move(210, 355)
+        self.save_button.resize(70, 45)
 
-        self.sync_button = QPushButton('Sync', self)
-        self.sync_button.move(605, 350)
+        self.sync_button = QPushButton('Sync \nSubs', self)
+        self.sync_button.move(420, 355)
+        self.sync_button.resize(70, 45)
 
         self.progress = QProgressBar(self)
-        self.progress.setGeometry(5, 300, 400, 40)
+        self.progress.move(5, 335)
+        self.progress.resize(485, 15)
 
 
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
-        self.setGeometry(535, 150, 210, 50)
+        
         if len(sys.argv) == 1+2:
-            self.file_name = sys.argv[1].replace('.mp4', '').replace('.avi', '').replace('.mkv', '')
+            self.file_name = sys.argv[1].replace('.mp4', '')\
+                                        .replace('.avi', '')\
+                                        .replace('.mkv', '')
             self.curr_time = sys.argv[2]
             self.get_srts()
             self.start_BigWindow()
@@ -100,19 +110,19 @@ class MainWindow(QMainWindow):
             self.start_BigWindow()
 
     def start_BigWindow(self):
-        self.resize(1000, 400)
+        self.resize(500, 410)
         self.move(100, 150)
-        self.ToolTab = BigWindow(self)
+        self.big_window = BigWindow(self)
         self.setWindowTitle("Sub-Learner")
-        self.setCentralWidget(self.ToolTab)
+        self.setCentralWidget(self.big_window)
 
-        self.ToolTab.line.returnPressed.connect(self.save_method)
-        self.ToolTab.prv_button.clicked.connect(self.go_previous)
-        self.ToolTab.nxt_button.clicked.connect(self.go_next)
-        self.ToolTab.apr_button.clicked.connect(self.add_previous)
-        self.ToolTab.anx_button.clicked.connect(self.add_next)
-        self.ToolTab.sv_button.clicked.connect(self.save_method)
-        self.ToolTab.sync_button.clicked.connect(self.sync_subtitles)
+        self.big_window.line.returnPressed.connect(self.save_method)
+        self.big_window.jumpPrevious_button.clicked.connect(self.go_previous)
+        self.big_window.jumpNext_button.clicked.connect(self.go_next)
+        self.big_window.appendPrevious_button.clicked.connect(self.add_previous)
+        self.big_window.appendNext_button.clicked.connect(self.add_next)
+        self.big_window.save_button.clicked.connect(self.save_method)
+        self.big_window.sync_button.clicked.connect(self.sync_subtitles)
 
         self.show()
 
@@ -139,9 +149,8 @@ class MainWindow(QMainWindow):
     def get_srt_object(self, lang):
         if lang not in ('ger', 'eng'):
             raise RuntimeError('language must be german or english')
-
-        # TODO split into two functions
     
+        # find srt files
         if "S0" in self.file_name:
             name_show, number_episode = self.file_name.split()
             search_pattern = f'{name_show}*{number_episode}*{lang}*.srt'
@@ -151,7 +160,8 @@ class MainWindow(QMainWindow):
         
         for file in glob.glob(str(search_path)):
             srt_file_path = file
-            
+        
+        # read srt objects
         try:
             srt_object = pysrt.open(srt_file_path)
         except UnicodeDecodeError: # UnicodeEncodeError:
@@ -167,7 +177,6 @@ class MainWindow(QMainWindow):
                                                                              slice_indexes=self.slice_indexes_deu,
                                                                              jump_to=jump_to,
                                                                              mode=mode)
-
         current_slice_eng, self.slice_indexes_eng = get_phrase_text(srt_object=self.subs_en,
                                                                              slice_indexes=self.slice_indexes_eng,
                                                                              jump_to=jump_to,
@@ -177,21 +186,20 @@ class MainWindow(QMainWindow):
 
     def update_progress(self):
         progress = (max(self.slice_indexes_deu)/len(self.subs_de))*100
-        self.ToolTab.progress.setValue(int(progress))
+        self.big_window.progress.setValue(int(progress))
 
     def go_next(self):
         # go to next sub_de
         jump_to = 'next'
         mode = 'replace'
         current_slice_deu, self.slice_indexes_deu = get_phrase_text(srt_object=self.subs_de,
-                                                                             slice_indexes=self.slice_indexes_deu,
-                                                                             jump_to=jump_to,
-                                                                             mode=mode)
-
+                                                                    slice_indexes=self.slice_indexes_deu,
+                                                                    jump_to=jump_to,
+                                                                    mode=mode)
         current_slice_eng, self.slice_indexes_eng = get_phrase_text(srt_object=self.subs_en,
-                                                                             slice_indexes=self.slice_indexes_eng,
-                                                                             jump_to=jump_to,
-                                                                             mode=mode)
+                                                                    slice_indexes=self.slice_indexes_eng,
+                                                                    jump_to=jump_to,
+                                                                    mode=mode)
 
         self.update_view(current_slice_deu, current_slice_eng)
 
@@ -199,14 +207,13 @@ class MainWindow(QMainWindow):
         jump_to = 'previous'
         mode = 'append'
         current_slice_deu, self.slice_indexes_deu = get_phrase_text(srt_object=self.subs_de,
-                                                                             slice_indexes=self.slice_indexes_deu,
-                                                                             jump_to=jump_to,
-                                                                             mode=mode)
-
+                                                                    slice_indexes=self.slice_indexes_deu,
+                                                                    jump_to=jump_to,
+                                                                    mode=mode)
         current_slice_eng, self.slice_indexes_eng = get_phrase_text(srt_object=self.subs_en,
-                                                                             slice_indexes=self.slice_indexes_eng,
-                                                                             jump_to=jump_to,
-                                                                             mode=mode)
+                                                                    slice_indexes=self.slice_indexes_eng,
+                                                                    jump_to=jump_to,
+                                                                    mode=mode)
 
         self.update_view(current_slice_deu, current_slice_eng)
 
@@ -214,22 +221,21 @@ class MainWindow(QMainWindow):
         jump_to = 'next'
         mode = 'append'
         current_slice_deu, self.slice_indexes_deu = get_phrase_text(srt_object=self.subs_de,
-                                                                             slice_indexes=self.slice_indexes_deu,
-                                                                             jump_to=jump_to,
-                                                                             mode=mode)
-
+                                                                    slice_indexes=self.slice_indexes_deu,
+                                                                    jump_to=jump_to,
+                                                                    mode=mode)
         current_slice_eng, self.slice_indexes_eng = get_phrase_text(srt_object=self.subs_en,
-                                                                             slice_indexes=self.slice_indexes_eng,
-                                                                             jump_to=jump_to,
-                                                                             mode=mode)
+                                                                    slice_indexes=self.slice_indexes_eng,
+                                                                    jump_to=jump_to,
+                                                                    mode=mode)
 
         self.update_view(current_slice_deu, current_slice_eng)
 
     def update_view(self, current_sub_de, current_sub_en):
-        self.ToolTab.Deu_cont.clear()
-        self.ToolTab.Eng_cont.clear()
-        self.ToolTab.Deu_cont.insertPlainText(clean_subtitle(current_sub_de))
-        self.ToolTab.Eng_cont.insertPlainText(clean_subtitle(current_sub_en))
+        self.big_window.Deu_cont.clear()
+        self.big_window.Eng_cont.clear()
+        self.big_window.Deu_cont.insertPlainText(clean_subtitle(current_sub_de))
+        self.big_window.Eng_cont.insertPlainText(clean_subtitle(current_sub_en))
         self.update_progress()
         self.show()
 
@@ -238,15 +244,15 @@ class MainWindow(QMainWindow):
         # updated
         print('save_method')
 
-        german_example = self.ToolTab.Deu_cont.toPlainText()
+        german_example = self.big_window.Deu_cont.toPlainText()
         german_example = format_example(video_title=self.file_name,
-                                          example=german_example)
-        english_example = self.ToolTab.Eng_cont.toPlainText()
+                                        example=german_example)
+        english_example = self.big_window.Eng_cont.toPlainText()
         english_example = format_example(video_title=self.file_name,
-                                          example=english_example)
+                                         example=english_example)
 
         print('Executing Command')
-        command_str = (f'python3 {script_path} -w "{self.ToolTab.line.text()}" -g "{german_example}" -e "{english_example}"')
+        command_str = (f'python3 {script_path} -w "{self.big_window.line.text()}" -g "{german_example}" -e "{english_example}"')
         print(command_str)
         os.popen(command_str)
 
--- open_sub_in_dict/TreatSubtitles.py
+++ open_sub_in_dict/TreatSubtitles.py
@@ -1,13 +1,6 @@
-def clean_subtitle(subs):
-    subs = subs.replace('</i>', '')\
-        .replace('<i>', '')\
-        .replace('– –', '–')\
-        .replace('-', '–')\
-        .replace('– –', '–')\
-        .replace("\n", " ")
-    return subs
+import pysrt
 
-def fetch_subs_from_timestamp(srt_object, minute, second):
+def fetch_subs_from_timestamp(srt_object: pysrt.srtitem.SubRipItem, minute: str, second: str) -> tuple[str, list]:
     '''progressivly expand time window until a subtitle is found'''
     for time_window in range(1, 60):
         subtitle_slice = srt_object.slice(starts_before={'minutes': int(minute),
@@ -26,7 +19,7 @@ def fetch_subs_from_timestamp(srt_object, minute, second):
     slice_text = ''.join([f' – {subtitle_phrase.text}' for subtitle_phrase in subtitle_slice])
     return slice_text, index_list
 
-def get_phrase_text(srt_object, slice_indexes: list, jump_to:str, mode: str):
+def get_phrase_text(srt_object: pysrt.srtitem.SubRipItem, slice_indexes: list, jump_to: str, mode: str) -> tuple[str, list]:
     if jump_to=='next' and mode=='replace':
         slice_indexes = [max(slice_indexes)+1]
     elif jump_to=='previous' and mode=='replace':
@@ -42,9 +35,18 @@ def get_phrase_text(srt_object, slice_indexes: list, jump_to:str, mode: str):
 
     return slice_text, slice_indexes
 
-def format_example(video_title, example: str) -> str:
+def format_example(video_title: str, example: str) -> str:
     # updated
     example = example.replace("'", "//QUOTE").replace('"', "//DOUBLEQUOTE")
     example += f' ({video_title})'
     example = example.strip()
     return example
+
+def clean_subtitle(subs: str) -> str:
+    subs = subs.replace('</i>', '')\
+               .replace('<i>', '')\
+               .replace('– –', '–')\
+               .replace('-', '–')\
+               .replace('– –', '–')\
+               .replace("\n", " ")
+    return subs
