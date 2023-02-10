1e34a22ba7807d0140184603acfe1f31894f8584
Avdic Sabahudin <sabahudin.avdic@park-depot.de>
2023-02-10 18:57:06 +0100

    show last 2 stacks in the error notif window

--- german_active_vocab/MainWindow.py
+++ german_active_vocab/MainWindow.py
@@ -1,4 +1,4 @@
- # import setuptools
+from logging import Logger
 import traceback
 from plyer import notification
 from PyQt5.QtCore import (Qt, QPropertyAnimation,
@@ -20,19 +20,25 @@ from QuizWindow import QuizWindow
 from DefEntry import DefEntry
 from utils import get_command_line_args, set_up_logger
 from another_qthread import Worker
+from settings import DICT_SRC_PATH
 
 # from autologging import traced
 
 # TODO (1) use rootword for filename and database enteries, different flexions of the word are now saved separetly
 # TODO (1) separate Wendungen, Redensarten, Sprichwörter in different headers for duden_dict (example absehen)
-# TODO (0) choose carefully your code helpers: mypy for type hinting, mccbee
+# TODO (1) choose carefully your code helpers:
+#   (prospector for now)
+#   mypy for type hinting
+#   mccbee(complexity checker, available in flake 8)
+#   pylint (general)
+#   black (formatting)
 # TODO (4) delete all out-commented code
 
-logger = set_up_logger(__name__)
+logger: Logger = set_up_logger(__name__)
 
 
 class MainWindow(QMainWindow):
-    def __init__(self, parent=None):
+    def __init__(self, parent=None) -> None:
         logger.info("init MainWindow")
         super(MainWindow, self).__init__(parent)
 
@@ -51,7 +57,7 @@ class MainWindow(QMainWindow):
         cl_args = get_command_line_args()
         self.launch_search_window(cl_args)
             
-    def launch_search_window(self, cl_args):
+    def launch_search_window(self, cl_args) -> None:
         logger.info("Launch search window")
         
         self.search_form = SearchWindow(parent=self)
@@ -68,7 +74,7 @@ class MainWindow(QMainWindow):
         
         self.show()
 
-    def expand_search_window_animation(self):
+    def expand_search_window_animation(self) -> None:
         logger.info("expand_window")
         current_width = self.width()
         window_x = self.x()
@@ -90,13 +96,13 @@ class MainWindow(QMainWindow):
         self.animation.setEndValue(QRect(window_x, window_y, end_width, 40))
         self.animation.start()
 
-    def launch_definition_window(self, def_obj):
+    def launch_definition_window(self, def_obj) -> None:
         logger.info("launch definition window")
 
         self.def_window = DefinitionWindow(def_obj=def_obj,
                                            parent=self)
 
-        self.def_window.construct_model(def_obj)
+        self.def_window.construct_model(def_obj)  # TODO shouldn be here if you're using def_obj in DefinitionWiDow
         self.def_window.fill_def_window(def_obj) # BUG (0) thabet fel unpacking thaherli mayhemouch esm el variable
         self.set_window_properties(title="Wörterbuch",
                                    central_widget=self.def_window,
@@ -104,7 +110,7 @@ class MainWindow(QMainWindow):
                                    size=QSize(self.def_window.base_width, 690))
         self.show()
 
-    def process_data_in_thread(self, cl_args=None):
+    def process_data_in_thread(self, cl_args=None) -> None:
         logger.info("process_data_in_thread")
 
         # Pass the function to execute
@@ -112,7 +118,7 @@ class MainWindow(QMainWindow):
         
         # Execute
         self.search_form.start_loading_animation()
-        # TODO grey out and setreadonly line edit
+        # TODO (1) LOOK&FEEL grey out and setreadonly line edit
         self.threadpool.start(worker)
 
         # connect signals and slots from second thread
@@ -122,16 +128,16 @@ class MainWindow(QMainWindow):
         worker.signals.message_box_content_carrier.connect(self.launch_message_box)
         # worker.signals.progress.connect(self.progress_fn)
 
-    def get_def_obj(self, cl_args, message_box_content_carrier):
+    def get_def_obj(self, cl_args, message_box_content_carrier) -> DefEntry:
         input_word = cl_args.word if cl_args and cl_args.word else self.search_form.get_filled_search_form()
         def_obj = DefEntry(input_word=input_word, cl_args=cl_args,
                            message_box_content_carrier=message_box_content_carrier,
-                           wait_for_usr=self.wait_condition) # (0)**** kammel lenna esta3mel wait condition bech tpausi el code mba3ed ki fama signal emitted yes wala no a3mel wait_condition wake all w khouth signal w kammel
+                           wait_for_usr=self.wait_condition)
         return def_obj
 
     @pyqtSlot(dict)
-    def launch_message_box(self, message_box_info):
-        "Asks the user about source and traget language for translation on receipt of a signal then signal a bool answer.`"
+    def launch_message_box(self, message_box_info) -> None:
+        "Asks the user about source and target language for translation on receipt of a signal then signal a bool answer.`"
         msg = QMessageBox()
         msg.setIcon(QMessageBox.Question)
         msg.setText(message_box_info['text'])
@@ -141,17 +147,19 @@ class MainWindow(QMainWindow):
         no_button = msg.addButton(message_box_info['no_button_text'], QMessageBox.NoRole)
         msg.exec_()
         
-        # hacky, but didn't find an easy way to intercept button signal after pausing code
+        # send clicked button to (paused) GenerateDict in worker thread
+        # hacky, but didn't find an easy way to intercept button signal
         from GetDict import GenerateDict
         GenerateDict.CLICKED_BUTTON = [msg.clickedButton() == yes_button,
-                          msg.clickedButton() == no_button]
+                                       msg.clickedButton() == no_button]
+        # unpause worker thread
         self.wait_condition.wakeAll()
 
-    def return_clicked(self):
+    def return_clicked(self) -> None:
         self._end_test = True
         QApplication.quit()
 
-    def expand_definition_window_animation(self):
+    def expand_definition_window_animation(self) -> None:
         logger.info("expand_window")
         current_width = self.width()
         window_x = self.x()
@@ -172,7 +180,7 @@ class MainWindow(QMainWindow):
         self.animation.setEndValue(QRect(window_x, window_y, end_width, 690))
         self.animation.start()
 
-    def launch_history_list_window(self):
+    def launch_history_list_window(self) -> None:
         logger.info("launch history list window")
 
         # TODO (3) allow modifing html in history Window
@@ -187,11 +195,10 @@ class MainWindow(QMainWindow):
 
         self.show()
 
-    def show_html_from_history_list(self, index):
+    def show_html_from_history_list(self, index) -> None:
         logger.info("show_html_from_history_list")
 
-        self.wordlist_window.returned_index = index
-        self.history_window = HistoryWindow(self)  # Why?, should be same as def window just with a different return action
+        self.history_window = HistoryWindow(parent=self, index=index)  # Why?, should be same as def window just with a different return action
 
         self.set_window_properties(title="Saved Words",
                                    central_widget=self.history_window,
@@ -199,7 +206,7 @@ class MainWindow(QMainWindow):
                                    size=QSize(700, 700))
         self.show()
 
-    def launch_quiz_window(self):
+    def launch_quiz_window(self) -> None:
         logger.info("launch_quiz_window")
 
         self.quiz_window = QuizWindow(self)
@@ -211,7 +218,7 @@ class MainWindow(QMainWindow):
 
         self.show()
 
-    def launch_focus_window(self):
+    def launch_focus_window(self) -> None:
         logger.info("launch_focus_window")
 
         self.focus_window = FocusWindow(self)
@@ -223,7 +230,7 @@ class MainWindow(QMainWindow):
 
         self.show()
 
-    def set_window_properties(self, title, central_widget, frameless, size):
+    def set_window_properties(self, title, central_widget, frameless, size) -> None:
         self.setWindowTitle(title)
         # Each time you use setCentralWidget(), the object that was previously the central widget is deleted.
         self.setCentralWidget(central_widget)
@@ -231,7 +238,7 @@ class MainWindow(QMainWindow):
         self.resize(size)
         self.move_to_center()
 
-    def move_to_center(self):
+    def move_to_center(self) -> None:
         # TODO (5) BUG move is not working at all in pyqt6!
         logger.info("move_to_center")
         frame_geometry=self.frameGeometry()           
@@ -242,38 +249,52 @@ class MainWindow(QMainWindow):
         self.centered_pos = frame_geometry.topLeft()
         self.move(self.centered_pos)
 
-    def set_shortcuts(self):
+    def set_shortcuts(self) -> None:
         self.shortcut_close = QShortcut(QKeySequence('Ctrl+Q'), self)
         self.shortcut_close.activated.connect(QApplication.quit)
 
 @pyqtSlot(object, object, object)
-def excepthook(exc_type, exc_value, exc_tb):
-    #TODO (1) show the right error format in notif
+def excepthook(exc_type, exc_value, exc_tb) -> None:
+    # DONE (1) show the right error format in notif
     # https://docs.python.org/3/library/traceback.html#traceback-examples
     traceback_message = "".join(traceback.format_exception(exc_type, exc_value, exc_tb))
-    print("error catched!:")
     print("error message:\n", traceback_message)
-    notification.notify(title='An Error Occured',
-                        message=str(exc_value.args[0]),
+
+    before_last_stack = traceback.format_tb(exc_tb, limit=-2)[0]
+    before_last_stack = before_last_stack.split(',')
+    before_last_stack[0] = before_last_stack[0].replace(str(DICT_SRC_PATH), '')
+    before_last_stack = ', '.join(before_last_stack)
+
+    last_stack = traceback.format_tb(exc_tb, limit=-1)[0]
+    last_stack = last_stack.split(',')
+    last_stack[0] = last_stack[0].replace(str(DICT_SRC_PATH), '')
+    last_stack = ', '.join(last_stack)
+
+    traceback_for_notif= (f'{exc_value.args[0]}'
+                           '\n--------\n'
+                          f'{before_last_stack}\n{last_stack}')
+
+    notification.notify(title=f'An Error Occured: {exc_value.__class__.__name__}',
+                        message=str(traceback_for_notif),
                         timeout=10)
 
     # show error in Qt MessageBox. was working, not anymore
     msg = QMessageBox()
     msg.setIcon(QMessageBox.Critical)
-    msg.setText("An Error Occured")
-    msg.setInformativeText(str(exc_value.args[0]))
+    msg.setText(f'An Error Occured: {exc_value.__class__.__name__}')
+    msg.setInformativeText(traceback_for_notif)
     msg.setWindowTitle("Error")
     msg.exec_()
 
     QApplication.quit() # or sys.exit(0)?
 
-def set_theme(app):
+def set_theme(app) -> None:
     # import platform
     # import darkdetect
     # import qdarkstyle
     # from darktheme.widget_template import DarkPalette
 
-    # TODO (3) find os-compatible themes
+    # TODO (3) LOOK&FEEL find os-compatible themes
     # I'm using (adwaita-qt in ubuntu or maybe qt5ct)
     # tried qdarkstyle (blueisch)
     # this one is close enough
